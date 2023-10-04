0304ad6dea898a6d72cac0149776cb40f48d2184
omiki235 <canomiki05@gmail.com>
2023-10-04 14:42:51 +0900

    feat: モックアップの実装

--- static/main.js
+++ static/main.js
@@ -12,3 +12,35 @@ const INITIAL_BOARD = [
   [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
   [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
 ];
+
+const boardElement = document.getElementById('board');
+
+async function showBoard() {
+  while (boardElement.firstChild) {
+    boardElement.removeChild(boardElement.firstChild);
+  }
+
+  INITIAL_BOARD.forEach((line) => {
+    line.forEach((square) => {
+      //8×8のマス
+      const squareElement = document.createElement('div');
+      squareElement.className = 'square';
+
+      if (square !== EMPTY) {
+        //黒い石
+        const stoneElement = document.createElement('div');
+        const color = square === DARK ? 'dark' : 'light';
+        stoneElement.className = `stone ${color}`;
+
+        squareElement.appendChild(stoneElement);
+      }
+      boardElement.appendChild(squareElement);
+    });
+  });
+}
+
+async function main() {
+  await showBoard();
+}
+
+main();
