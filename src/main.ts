63e71d0c4eb7393aa7bf63fdda1b2cb26bda4c4f
omiki235 <canomiki05@gmail.com>
2023-10-04 11:40:39 +0900

    morganとエラーハンドリング設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+import 'express-async-errors';
 
 const PORT = 8000;
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/api/hello', async (req, res) => {
   res.json({
@@ -10,6 +14,24 @@ app.get('/api/hello', async (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Webサーバー: http://localhost:${8000}`);
+app.get('/api/error', async (req, res) => {
+  throw new Error('Error Endpoint');
 });
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`サーバー起動: http://localhost:${PORT}`);
+});
+
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error('予期しないエラーが発生しました', err);
+  res.status(500).send({
+    message: '予期しないエラーが発生しました',
+  });
+}
