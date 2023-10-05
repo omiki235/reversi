bfefc32def272705837b4f05c67d4be523a5bb85
omiki235 <canomiki05@gmail.com>
2023-10-05 16:16:54 +0900

    feat: 盤面を取得する実装

--- src/main.ts
+++ src/main.ts
@@ -32,12 +32,7 @@ app.get('/api/error', async (req, res) => {
 app.post('/api/games', async (req, res) => {
   const now = new Date();
 
-  const conn = await mysql.createConnection({
-    host: 'localhost',
-    database: 'reversi',
-    user: 'reversi',
-    password: 'password',
-  });
+  const conn = await connecMySQL();
   try {
     await conn.beginTransaction();
 
@@ -84,6 +79,47 @@ app.post('/api/games', async (req, res) => {
   res.status(201).end();
 });
 
+app.get('/api/games/latest/turns/:turnCount', async (req, res) => {
+  const turnCount = parseInt(req.params.turnCount);
+
+  const conn = await connecMySQL();
+  try {
+    const gameSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, started_at from games order by id desc limit 1'
+    );
+
+    const game = gameSelectResult[0][0];
+
+    const turnSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, game_id, turn_count, next_disc, end_at from turns where game_id = ? and turn_count = ?',
+      [game['id'], turnCount]
+    );
+    const turn = turnSelectResult[0][0];
+
+    const squaresSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, turn_id, x, y, disc from squares where turn_id = ?',
+      [turn['id']]
+    );
+
+    const squares = squaresSelectResult[0];
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
+    squares.forEach((s) => {
+      board[s.y][s.x] = s.disc;
+    });
+
+    const resposeBody = {
+      turnCount,
+      board,
+      nextDisc: turn['next_disc'],
+      // TODO 決着がついている場合、 game_resultsテーブルから取得する
+      winnerDisc: null,
+    };
+    res.json(resposeBody);
+  } finally {
+    await conn.end();
+  }
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
@@ -99,5 +135,14 @@ function errorHandler(
   console.error('Unexpected error occurred', err);
   res.status(500).send({
     message: 'Unexpected error occurred',
+  });
+}
+
+async function connecMySQL() {
+  return await mysql.createConnection({
+    host: 'localhost',
+    database: 'reversi',
+    user: 'reversi',
+    password: 'password',
   });
 }
