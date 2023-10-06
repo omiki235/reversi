4d76cac6cec6e9e30f8734bf8a30c7c866ba8947
omiki235 <canomiki05@gmail.com>
2023-10-06 11:21:06 +0900

    feat: フロントエンドの石を打つ処理

--- static/main.js
+++ static/main.js
@@ -1,54 +1,71 @@
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
 
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-]
-
-const boardElement = document.getElementById('board')
+const boardElement = document.getElementById('board');
 
 async function showBoard() {
+  const turnCount = 0;
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
+
   while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild)
+    boardElement.removeChild(boardElement.firstChild);
   }
 
-  INITIAL_BOARD.forEach((line) => {
+  board.forEach((line) => {
     line.forEach((square) => {
       // <div class="square">
-      const squareElement = document.createElement('div')
-      squareElement.className = 'square'
+      const squareElement = document.createElement('div');
+      squareElement.className = 'square';
 
       if (square !== EMPTY) {
         // <div class="stone dark">
-        const stoneElement = document.createElement('div')
-        const color = square === DARK ? 'dark' : 'light'
-        stoneElement.className = `stone ${color}`
+        const stoneElement = document.createElement('div');
+        const color = square === DARK ? 'dark' : 'light';
+        stoneElement.className = `stone ${color}`;
 
-        squareElement.appendChild(stoneElement)
+        squareElement.appendChild(stoneElement);
+      } else {
+        squareElement.addEventListener('click', () => {
+          console.log('clicked');
+        });
       }
 
-      boardElement.appendChild(squareElement)
-    })
-  })
+      boardElement.appendChild(squareElement);
+    });
+  });
 }
 
 async function registerGame() {
   await fetch('/api/games', {
-    method: 'POST'
-  })
+    method: 'POST',
+  });
+}
+
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch('/api/games/latest/turns', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  });
 }
 
 async function main() {
-  await registerGame()
-  await showBoard()
+  await registerGame();
+  await showBoard();
 }
 
-main()
+main();
